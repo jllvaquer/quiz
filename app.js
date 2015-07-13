86caa5b375a9320ce61af9d06bfe2ff3f3c509c4
Josep Lluís Vaquer <jll.vaquer@gmail.com>
2015-07-13 16:44:56 +0200

    Marco de la aplicación

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var path = require('path');
-//var favicon = require('serve-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var partials = require('express-partials');
 var routes = require('./routes/index');
 //var users = require('./routes/users');
 
@@ -14,8 +15,10 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(partials());
+
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
