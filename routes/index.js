5da89c3256ae72109a202513f46162b8c1892b45
Josep Lluís Vaquer <jll.vaquer@gmail.com>
2015-07-27 12:14:52 +0200

    Añadir preguntas

--- routes/index.js
+++ routes/index.js
@@ -17,5 +17,7 @@ router.param('quizId', quizController.load);
 router.get('/quizes',						quizController.index);
 router.get('/quizes/:quizId(\\d+)',			quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',	quizController.answer);
+router.get('/quizes/new',					quizController.new);
+router.get('/quizes/create',				quizController.create);
 
 module.exports = router;
