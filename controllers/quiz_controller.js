a80257292f313ad53147708b719645cfb22cb514
Josep Lluís Vaquer <jll.vaquer@gmail.com>
2015-07-28 10:50:19 +0200

    Temas en las preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -17,6 +17,10 @@ exports.index = function(req, res) {
 		models.Quiz.findAll({where: ["pregunta like ?", search]}).then(function(quizes){
 			res.render('quizes/index.ejs', {quizes: quizes, errors: []});
 		})
+	} else if(req.query.tema){
+		models.Quiz.findAll({where: ["tema like ?", req.query.tema]}).then(function(quizes){
+			res.render('quizes/index.ejs', {quizes: quizes, errors: []});
+		})
 	} else {
 		models.Quiz.findAll().then(function(quizes) {
 			res.render('quizes/index.ejs', {quizes: quizes, errors: []});
@@ -42,7 +46,7 @@ exports.answer = function(req,res) {
 
 exports.new = function(req,res) {
 	var quiz = models.Quiz.build(
-	{pregunta: "Pregunta", respuesta: "Respuesta"}
+	{ pregunta: "Pregunta", respuesta: "Respuesta", tema: "Tema" }
 	);
 	res.render('quizes/new', {quiz: quiz, errors: []});
 };
@@ -56,7 +60,7 @@ exports.create = function(req,res) {
 			if(err){
 				res.render('quizes/new', {quiz: quiz, errors: err.errors});
 			} else {
-				quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+				quiz.save({fields: ["pregunta", "respuesta", "tema"]}).then(function(){
 				res.redirect('/quizes');
 				})
 			}
@@ -72,6 +76,7 @@ exports.edit = function(req, res) {
 exports.update = function(req, res) {
 	req.quiz.pregunta = req.body.quiz.pregunta;
 	req.quiz.respuesta = req.body.quiz.respuesta;
+	req.quiz.tema = req.body.quiz.tema;
 	
 	req.quiz
 	.validate()
@@ -81,7 +86,7 @@ exports.update = function(req, res) {
 				res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
 			} else {
 				req.quiz
-				.save({fields: ["pregunta", "respuesta"]})
+				.save({fields: ["pregunta", "respuesta", "tema"]})
 				.then(function(){ res.redirect('/quizes');});
 			}
 		});
