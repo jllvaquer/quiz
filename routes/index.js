50a764c51c53835c844217d3cc9a08cc73a6b4f8
Josep Lluís Vaquer <jll.vaquer@gmail.com>
2015-07-29 19:13:18 +0200

    Validación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,7 @@ router.get('/author', function(req, res) {
 });
 
 router.param('quizId', quizController.load);
+router.param('commentId', commentController.load);
 
 router.get('/login',	sessionController.new);
 router.post('/login',	sessionController.create);
@@ -31,5 +32,6 @@ router.delete('/quizes/:quizId(\\d+)',		sessionController.loginRequired, quizCon
 
 router.get('/quizes/:quizId(\\d+)/comments/new',	commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',		commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
